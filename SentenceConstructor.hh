9c9671039eb9e94d9c03031d4bbadbd532d6341d
RoryUD <attyqwerty12@gmail.com>
2024-03-02 13:09:35 -0500

    Update SentenceConstructor.hh
    Added constructor and getter/setter

--- SentenceConstructor.hh
+++ SentenceConstructor.hh
@@ -15,13 +15,46 @@ using namespace std;
  * 		Have word bank of moveable blocks
  * 		Create 10 ish sentences
  * 		Front end stuff 
- *  	
-*/
+ * 		
+ *  	g++ main.cpp SentenceConstructor.hh -o main
+ * 		main
+ */
 
-class Dungeon
+class SentenceConstructor
 {
 	
-}
+};
 
+class WordBlock
+{
+public: 
+	WordBlock(string, int);		//Full constructor 
+	WordBlock(string); 			//Just sentence constructor 
+	void setContent(string);
+	void setPlaceOrder(int);
+	string getContent();
+	int getPlaceOrder();
+	
+private:
+	int placeOrder;
+	string content;
+};
+//Constructors
+WordBlock::WordBlock(string sentence)
+{
+	content = sentence;
+	placeOrder = -1;
+}
+WordBlock::WordBlock(string sentence, int order)
+{
+	content = sentence;
+	placeOrder = order;
+}
+//Setters and Getters
+void WordBlock::setContent(string sentence){ content = sentence;}
+void WordBlock::setPlaceOrder(int num){ placeOrder = num;}
+
+string WordBlock::getContent(){ return content;}
+int WordBlock::getPlaceOrder(){ return placeOrder;}
 
 #endif
