2a6ccdf584b7d832eb22e1a21d7b4ecc27264833
Alex Hoy <aghoy03@gmail.com>
2024-03-03 09:51:27 -0500

    Fixed merge issues

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App(): JSX.Element {
         <div className="App">
             <h1>Gender Inclusive Language Examples</h1>
             <header className="App-header">
-                Interactive Examples to enhance language for diversity:
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Gay_Pride_Flag.svg/800px-Gay_Pride_Flag.svg.png" style={{width: 1600, height: 200}} alt="Pride Flag"></img>
             </header>
             <p >
                 This site is designed to teach you about gendered langauge!
